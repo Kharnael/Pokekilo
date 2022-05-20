b7367d73c1cbb0264aa923085765749d98284bfa
QuentinParsy <parsyquentin@gmail.com>
2022-05-20 14:45:09 +0200

    add filter

--- public/assets/js/filter.js
+++ public/assets/js/filter.js
@@ -1,2 +1,12 @@
-const select = document.querySelectorAll('.dropdown-menu li img')
-console.log(select)
+// filter by type
+const selects = document.querySelectorAll('.dropdown-menu li img')
+
+for (i = 0; i < selects.length; i++) {
+    selects[i].addEventListener('click', (e) => {  
+        e.preventDefault();
+        const filter = e.target.dataset.type;
+        console.log(filter)
+
+
+    })
+}
