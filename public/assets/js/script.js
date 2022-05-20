7c5e1dc783dc9c4a4caf9cbfeb4bc5931ca9ad32
Jidess <102791310+Jidess@users.noreply.github.com>
2022-05-20 14:45:01 +0200

    daymode finito

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -25,15 +25,16 @@ fetchApi = async () => {
                 </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item bg-danger"><img src="public/assets/img/${pokeType}.png" alt="${pokeType} picture"></li>
-                    <li class="list-group-item bg-danger">${pokeDescription}</li>
+                    <li class="list-group-item bg-danger pokeDesc">${pokeDescription}</li>
                     <li class="list-group-item bg-danger">Poids : ${pokeWeight}kg</li>
                     <li class="list-group-item bg-danger">Pokédex N° : ${pokeId}</li>
                     <li class="list-group-item bg-danger">Prix : ${pokePrice}₽</li>
+                    <li class="list-group-item bg-danger"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi  bi-cart-plus-fill my-2 mx-auto cartAdd data-poke="${pokeName}" viewBox="0 0 16 16">
+                    <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+                  </svg></li>
                 </ul>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi  bi-cart-plus-fill my-2 mx-auto cartAdd data-poke="${pokeName}" viewBox="0 0 16 16">
-  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
-</svg>
+            
         </article>
  
         `;
