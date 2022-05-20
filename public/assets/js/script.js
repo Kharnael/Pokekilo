dfd17b73fb88137772d1257cd4ec6d427022ffde
Jidess <102791310+Jidess@users.noreply.github.com>
2022-05-20 09:48:55 +0200

    ajout du btn cart

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -16,8 +16,8 @@ fetchApi = async () => {
         let pokeName = data2.names[4].name;
         test.innerHTML += 
         `
-        <article class="col col-md-6 col-lg-3 d-flex justify-content-center my-4">
-            <div class="card bg-danger text-center">
+        <article class="col col-md-6 col-lg-3 d-flex justify-content-center flex-column my-4 ">
+            <div class="card bg-danger text-center mx-auto">
                 <img src="${imgPoke}" class="card-img-top" alt="${pokeName} picture">
                 <div class="card-body">
                     <h5 class="card-title">${pokeName}</h5>
@@ -31,7 +31,9 @@ fetchApi = async () => {
                     <li class="list-group-item bg-danger">Prix : ${pokePrice}₽</li>
                 </ul>
             </div>
-
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi  bi-cart-plus-fill my-2 mx-auto cartAdd " viewBox="0 0 16 16">
+  <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/>
+</svg>
         </article>
  
         `;
