ae26fdacd9779f4eb4ab07cc13e8d1d36f89f78e
Antho <maxister@gmail.com>
2022-05-20 10:44:38 +0200

    cartFunction

--- public/assets/js/addToCart.js
+++ public/assets/js/addToCart.js
@@ -1,13 +1,13 @@
 //Demarrage avec un array "cartArray à 0"
 let cartArray = [];
 
-//============== gestion Array/LocalStorage ===============================
+//======================= GESTION ARRAY / LOCALSTORAGE           ==================================
 
 //add to array
 addToArray = () => {
     let LSdata = JSON.parse(localStorage.getItem('cartData'));
     LSdata.forEach(element => {
-        cartArray.push({id:`${element.id}`, name:`${element.name}`, price:`${element.price}`, Qty:`${element.Qty}`}); //mettre dans le push les key/value du tablobjet 'cartArray'
+        cartArray.push({id:`${element.id}`, name:`${element.name}`, price:`${element.price}`, Qty:`${element.Qty}`, totalUnitPrice:`${element.totalUnitPrice}`}); //mettre dans le push les key/value du tablobjet 'cartArray'
     });
 }
 
@@ -18,11 +18,13 @@ addToLocalStorage = () => {
     localStorage.setItem('cartData', JSON.stringify(cartArray))
 }
 
+//======================= FIN DE GESTION ARRAY / LOCALSTORAGE     =================================
+
 // fonction ajouter au panier
 addToCart = () => {
-    let pokeId = 2;                 //value dans le HTML    => A supprimer une fois les values dispo dans le HTML
-    let pokeName = 'kikoo2';        //value dans le HTML   => A supprimer une fois les values dispo dans le HTML
-    let pokePrice = 22;             //value dans le HTML    => A supprimer une fois les values dispo dans le HTML
+    let pokeId = 1;                 //value dans le HTML    => A supprimer une fois les values dispo dans le DOM
+    let pokeName = 'kikoo';        //value dans le HTML   => A supprimer une fois les values dispo dans le DOM
+    let pokeUnitPrice = 15.5;             //value dans le HTML    => A supprimer une fois les values dispo dans le DOM
 
     // /!\ resultz permet de savoir si le pokemon est DEJA dans le panier. /!\
     let resultz = cartArray.find(nameOf => nameOf.name === pokeName);
@@ -30,30 +32,66 @@ addToCart = () => {
         //Si le tableau est pas vide et que le pokemon est déja dans le panier, alors ajoute 1 à Qty
     if (resultz != undefined) { 
                 cartArray.forEach(element => {
-                if (element.name == pokeName) {element.Qty ++;
-                    // on est des fous, on calcul le prix total en fonction de la quantité dans la fonction addtocart! 
-                    let newPrice = parseFloat(element.price) + parseFloat(pokePrice);
-                    element.price = newPrice;
-                }})
+                    if (element.name == pokeName) {
+                        element.Qty ++;
+                        //on calcul le prix total(par pokemon) en fonction de la quantité dans la fonction addtocart! 
+                        let newPrice = parseFloat(element.price) * parseFloat(element.Qty);
+                        element.totalUnitPrice = parseFloat(newPrice.toFixed(2));
+                    }
+                })
 
-        //Sinon (le pokemon est pas dans le panier) donc ajoute le avec une Qty de 1        
+    //Sinon (le pokemon est pas dans le panier donc) ajoute le avec une Qty de 1        
     } else if (resultz == undefined) { 
-        cartArray.push({id:`${pokeId}`, name:`${pokeName}`, price:`${pokePrice}`, Qty:1});
+        cartArray.push({id:`${pokeId}`, name:`${pokeName}`, price:`${pokeUnitPrice}`, Qty:1, totalUnitPrice:`${pokeUnitPrice}`});
     }
 addToLocalStorage();
 }
 
-//eventListener
-// idAddToCartFromHTML.addEventListener('click', addToCart())
-// idRemoveFromCartFromHTML.addEventLister('click' removeFromCart())
-
 //Calcul du prix total
-CalculOfTotalPrice = () => {
+calculOfTotalPrice = () => {
     let totalPrice = 0;
     cartArray.forEach(element => {
-        totalPrice += parseFloat(element.price);
+        totalPrice += parseFloat(element.totalUnitPrice);
     });
-    console.log(totalPrice);
+    return totalPrice.toFixed(2); //totalPrice DEVIENT UN STRING ATTENTION. le reparse en float si needed.
+}
+
+//eventListener
+// idAddToCartFromHTML.addEventListener('click', addToCart())
+// idRemoveFromCartOneByOne.addEventLister('click', removeOneByOne())
+// idRemoveFromCartOnePokeTotaly.addEventLister('click', removeOnePokeTotaly())
+// idDeleteAllCart.addEventLister('click', deleteAllCart())
+
+//Retirage d'un pokemon par un pokemon
+removeOneByOne = () => {
+    let selectedPokeToRemoveOne = "kikoo" //a changer, value provenant du DOM
+    cartArray.forEach(element => {
+        if (element.name == selectedPokeToRemoveOne) {
+            //si qty 1 et l'utilisateur veut enlever 1, c'est qu'il en veut plus du tout
+            if (element.Qty == 1) {removeOnePokeTotaly(selectedPokeToRemoveOne)}
+            else { 
+            element.Qty -= 1;
+            element.totalUnitPrice = element.Qty * element.price;}
+            }
+        });
+        addToLocalStorage(); // MAJ du LS.
+}
+
+//Retirage de toute les quantité d'un seul pokemon
+removeOnePokeTotaly = (selectedPokeToRemoveCompletely) => {
+    // let selectedPokeToRemoveCompletely = 'kikoo2';
+    cartArray.forEach((element, index) => {
+        if (selectedPokeToRemoveCompletely == element.name) {
+            cartArray.splice(index, 1);
+        }
+    });
+    addToLocalStorage();
+}
+
+//vidange total du panier 
+deleteAllCart = () => {
+    cartArray = [];
+    localStorage.removeItem('cartData');
 }
 
 // Au chargement de la page, si le local storage n'est pas vide
