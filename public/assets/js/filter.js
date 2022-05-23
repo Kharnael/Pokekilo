dea06a6d479dc277e467f79bde6ede3c09acce1a
QuentinParsy <parsyquentin@gmail.com>
2022-05-23 09:52:09 +0200

    ok

--- public/assets/js/filter.js
+++ public/assets/js/filter.js
@@ -1,12 +1,15 @@
 // filter by type
-// const selects = document.querySelectorAll('.dropdown-menu li img')
-
-// for (i = 0; i < selects.length; i++) {
-//     selects[i].addEventListener('click', (e) => {  
-//         e.preventDefault();
-//         const filter = e.target.dataset.type;
-//         console.log(filter)
+const selects = document.querySelectorAll('.dropdown-menu li img')
+// const pokéFilter = pokeCatalog.filter(pokeCatalog => pokeCatalog.length > 6)
+// console.log(pokéFilter)
 
 
-//     })
-// }
+for (i = 0; i < selects.length; i++) {
+    selects[i].addEventListener('click', (e) => {  
+        e.preventDefault();
+        const filter = e.target.dataset.type;
+        // console.log(filter)
+        
+
+    })
+}
