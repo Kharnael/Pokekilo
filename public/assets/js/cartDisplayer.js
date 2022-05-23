a5fd4a3863484b35c381f06a765e95722182fab7
Jidess <102791310+Jidess@users.noreply.github.com>
2022-05-23 11:08:30 +0200

    cart finito et pokéchargement

--- public/assets/js/cartDisplayer.js
+++ public/assets/js/cartDisplayer.js
@@ -3,7 +3,7 @@ pushToCartModal = () => {
     cartArray.forEach(element => {
         cartWrite.innerHTML +=
             `
-        <li class="border-bottom border-dark border-3">${element.name} <img src="${element.img}" alt="${element.name} picture"> <span class="removeOneBtn" data-pokeName="${element.name}">-</span> Qté: ${element.Qty} <span class="addOneBtn" data-pokeName="${element.name}">+</span> <div> Prix: ${element.totalUnitPrice}₽ <span class="removeAllQtyOfOnePoke" data-pokeName="${element.name}">[Supprimer le Pkmn]</span></div></li>
+        <li class="border-bottom border-dark border-3 fs-5 lh-lg">${element.name} <img src="${element.img}" alt="${element.name} picture"> <div class="text-center bg-dark text-light lh-lg"> <span class="removeOneBtn" data-pokeName="${element.name}">-</span> ${element.Qty} <span class="addOneBtn" data-pokeName="${element.name}">+</span></div> <div class="d-flex justify-content-between lh-lg"> ${element.totalUnitPrice}₽ <span class="removeAllQtyOfOnePoke text-end" data-pokeName="${element.name}">X</span></div></li>
         `;
     });
 
