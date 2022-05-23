c125c8c7f2678f3c6a2be9604f1853d1eadd9500
Antho <maxister@gmail.com>
2022-05-23 09:51:13 +0200

    new filtered array

--- public/assets/js/filter.js
+++ public/assets/js/filter.js
@@ -1,12 +1,20 @@
-// filter by type
-// const selects = document.querySelectorAll('.dropdown-menu li img')
+//filter by type
+const selects = document.querySelectorAll('.dropdown-menu li img')
+const filteredResults = []
 
-// for (i = 0; i < selects.length; i++) {
-//     selects[i].addEventListener('click', (e) => {  
-//         e.preventDefault();
-//         const filter = e.target.dataset.type;
-//         console.log(filter)
+for (i = 0; i < selects.length; i++) {
+    selects[i].addEventListener('click', (e) => {  
+        e.preventDefault();
+        const filter = e.target.dataset.type;
+        
+    })
+}
 
-
-//     })
-// }
+pokeFilter = () => {
+    pokeCatalog.forEach((element, index) => {
+        if(element.type == filter) {
+            filteredResults.push({name:element.name, id:element.id, type:element.type, desc:element.desc, price:element.price, sprite:element.sprite, weight:element.weight, icons:element.icons})
+        }
+    });
+    console.table(filteredResults);
+}
